b5a7c8e21769dae7a9c55534a69c5a3315d088a4
INFINITY <mishrasugam11@gmail.com>
2025-10-22 19:57:24 -0500

    .env

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -12,12 +12,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os 
+from dotenv import load_dotenv
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles' 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
